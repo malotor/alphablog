be8ec093b19799d07fd30e6b8af4f199f5cd3056
Manel <malotor@gmail.com>
2018-04-17 00:13:31 +0200

    Only admis can create categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,25 +1,19 @@
 require 'test_helper'
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: 'sports')
 
-    def setup
+    @category2 = Category.create(name: 'programming')
+  end
 
-        @category = Category.create(name: "sports")
+  test 'should show categories listing' do
+    get categories_path
 
-        @category2 = Category.create(name: "programming")
+    assert_template 'categories/index'
 
-    end
+    assert_select 'a[href=?]', category_path(@category), text: @category.name
 
-    test "should show categories listing" do
-
-        get categories_path
-
-        assert_template 'categories/index'
-
-        assert_select "a[href=?]", category_path(@category), text: @category.name
-
-        assert_select "a[href=?]", category_path(@category2), text: @category2.name
-
-    end
-
+    assert_select 'a[href=?]', category_path(@category2), text: @category2.name
+  end
 end
